2ee6638cfd428f890b55d41936d4a66d827e4ef3
Kenda Shanti <kshanti@si-i6.kzn.21-school.ru>
2021-05-16 20:25:58 +0300

    start getting one command

--- kshanti/srcs_parser/parser.c
+++ kshanti/srcs_parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 17:56:35 by kshanti           #+#    #+#             */
-/*   Updated: 2021/05/14 20:00:16 by kshanti          ###   ########.fr       */
+/*   Updated: 2021/05/16 20:24:03 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,10 +48,37 @@ void		preparser(char *commands_line)
 	}
 }
 
+void		skip_spases_tabs(char **str)
+{
+	while (**str == ' ' || **str == '\t')
+		str = *str + 1;
+}
+
+t_commands	*get_one_command(char **p_commands_line, char ** env)
+{
+	t_commands	*command;
+	char		*command_line;
+
+	skip_spases_tabs(p_commands_line);
+	command_line = *p_commands_line;
+	while (*command_line && *command_line != '\n' && *command_line != ';')
+	{
+		replace_single_quotes();//  '
+		replace_double_quotes();//  "
+		replace_dolar();//          $
+	}
+}
+
 t_commands	*parser(char *commands_line, char **env)
 {
+	t_commands		*first;
+
 	if (!commands_line)
 		error_control("Commands line is NULL");
-	preparser(commands_line);//
+	preparser(commands_line);
+	while (*commands_line)
+	{
+		ft_lstadd_back(&first, get_one_command(&commands_line, env));
+	}
 	return (NULL);
 }
