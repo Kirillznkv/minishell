b7658c3351d68cd1a44404ae113dcb71209f4bf9
troxanna <anevodchikova29@gmail.com>
2021-05-20 21:08:48 +0300

    v3

--- test_troxanna/includes/minishell.h
+++ test_troxanna/includes/minishell.h
@@ -5,6 +5,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef	struct 				s_env
+{
+	char					*key;
+	char					*value;
+	struct s_env			*next;
+}							t_env;
+
 void		ft_putchar(char c);
 int			ft_strlen(char *s);
 void		ft_putstr(char *s);
@@ -12,6 +19,7 @@ void		ft_error(int n);
 int			ft_strcmp(const char *str1, const char *str2);
 int			ft_isalpha(int ch);
 void		ft_env_shell(char **env);
+t_env		*ft_env_sort(t_env *env_export);
 void		ft_export_shell(char **env);
 char		*ft_strnstr(const char *s, const char *find, size_t slen);
 void		ft_pwd_shell(char **env);
--- test_troxanna/srcs/build_in/build_in_command.c
+++ test_troxanna/srcs/build_in/build_in_command.c
@@ -1,48 +1,24 @@
-#include "../includes/minishell.h"
-
-char		**malloc_env(char **env)
-{
-	int 	i;
-	int 	j;
-	char	**env_malloc;
-
-	i = 0;
-	j = 0;
-	while (env[i])
-		i++;
-	env_malloc = (char **)malloc(sizeof(char *) * i);
-	if (!env_malloc)
-		return (NULL);
-	while (env[i])
-	{
-		while (env[j])
-			j++;
-		env_malloc[i] = (char *)malloc(sizeof(char) * j + 1);
-		i--;
-	}
-	return (env_malloc);
-}	
-
-char		**ft_env_sort(char **env)
-{
-	int 	c[2];
-	char	**env_sorted;
+#include "../../includes/minishell.h"
 
 
-	c[0] = 0;
-	c[1] = 0;
-	env_sorted = malloc_env(env);
-	//Продумать, какой метод сортировки использовать
-	// if (ft_isalpha(env[c[0]][c[1]])
-	// {
-	// 	while (env[c[0]])
-	// 	{
-	// 		if (env[c[0] + 1] && env[c[0] + 1][c[1]] < env[c[0]][c[1]])
+// t_env			*ft_env_sort(t_env *env_export)
+// {
+// 	int			i;
+// 	char		*tmp;
+// 	t_env		*ptr;
 
-	// 	}
-	// }
-	return (env_sorted);
-}
+// 	i = 0;
+// 	tmp = NULL;
+// 	ptr = env_export;
+// 	while (ptr)
+// 	{
+// 		// if (ft_strcmp(ptr->key, ptr->next->key) < 0)
+// 		// {
+
+// 		// }
+// 		//сортировка по списку
+// 	}
+// }
 
 static char		*get_pwd(char **env)
 {
@@ -98,19 +74,6 @@ void	ft_pwd_shell(char **env)
 		show_pwd(str);
 }
 
-void	ft_export_shell(char **env)
-{
-	int i;
-
-	i = 0;
-	//ft_env_sort(env);
-	while (env[i])
-	{
-		ft_putstr("declare -x ");
-		ft_putstr(env[i++]);
-		ft_putchar('\n');
-	}
-}
 
 void	ft_env_shell(char **env)
 {
