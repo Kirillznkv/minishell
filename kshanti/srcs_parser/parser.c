7569da97aeeb2e0f3321a0255794043133cc916d
Kenda Shanti <kshanti@pr-m3.kzn.21-school.ru>
2021-05-25 21:26:11 +0300

    fix malloc argv, need fix leaks

--- kshanti/srcs_parser/parser.c
+++ kshanti/srcs_parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 17:56:35 by kshanti           #+#    #+#             */
-/*   Updated: 2021/05/25 20:59:48 by kshanti          ###   ########.fr       */
+/*   Updated: 2021/05/25 21:24:54 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void		replace_normal_char(char **p_command_line, size_t *i)
 		/*command_line[*i] != '|' && */command_line[*i] != '\t' &&
 		command_line[*i] != ';' && command_line[*i] != '\0' &&
 		command_line[*i] != '\n')
-		/*printf("norm char |%c|\n",command_line[*/(*i)++;//]);
+		(*i)++;
 }
 
 void		check_end_word(char **p_command_line, size_t *i, t_commands *command)
@@ -36,6 +36,7 @@ void		check_end_word(char **p_command_line, size_t *i, t_commands *command)
 		command_line[*i] == ';' || command_line[*i] == '\n' ||
 		command_line[*i] == '\0')
 	{
+		command->argv = malloc_argv(command->argc, command->argv);
 		command->argv[command->argc++] = ft_substr(*p_command_line, 0, *i);
 		skip_spases_tabs(p_command_line, *i);
 		save_to_free = *p_command_line;
@@ -55,7 +56,7 @@ t_commands	*get_one_command(char **p_commands_line, char **env)
 	command->argc = 0;
 	command->argv = NULL;
 	command->next = NULL;
-	command->argv = (char**)malloc(sizeof(char*) * 10);///// fix malloc
+	command->argv = NULL;
 	i = 0;
 	skip_spases_tabs(p_commands_line, i);
 	command_line = *p_commands_line;
