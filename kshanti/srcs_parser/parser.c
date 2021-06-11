43123e2ccdcc0a2661a1e3ff77d96284db6009b1
Kenda Shanti <kshanti@ge-m1.kzn.21-school.ru>
2021-06-11 17:00:20 +0300

    minishell-2.0$

--- kshanti/srcs_parser/parser.c
+++ kshanti/srcs_parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 17:56:35 by kshanti           #+#    #+#             */
-/*   Updated: 2021/06/10 21:26:55 by kshanti          ###   ########.fr       */
+/*   Updated: 2021/06/11 16:55:07 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,7 +124,8 @@ t_commands	*parser(char *commands_line, char **env)//; в начале
 	while (*commands_line)
 	{
 		command = get_one_command(&commands_line, env);
-		parse_command(command, env);
+		if (command)
+			parse_command(command, env);
 		// while (command)
 		// {
 		// 	int i = -1;
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 19:21:27 by kshanti           #+#    #+#             */
-/*   Updated: 2021/06/10 22:16:35 by kshanti          ###   ########.fr       */
+/*   Updated: 2021/06/11 16:58:02 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,11 @@ int		main(int argc, char **argv, char **env)
 	
 	char	*line;
 
+	write(1, "Minishell-2.0$ ", 16);
 	while (get_next_line(0, &line) == 1)
 	{
 		parser(line, env);
+		write(1, "Minishell-2.0$ ", 16);
 	}
 	return (0);
 }
