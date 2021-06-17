f58df44f18adc7a44593ff2bd9258f61d91958a7
Kenda Shanti <kshanti@ge-l2.kzn.21-school.ru>
2021-06-17 17:05:49 +0300

    delete printf comment

--- kshanti/srcs_parser/redirect.c
+++ kshanti/srcs_parser/redirect.c
@@ -6,7 +6,7 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 21:56:48 by kshanti           #+#    #+#             */
-/*   Updated: 2021/06/17 17:02:51 by kshanti          ###   ########.fr       */
+/*   Updated: 2021/06/17 17:05:08 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,7 +86,6 @@ void		back_redirect(t_commands *command, char **p_command_line, size_t *i)
 	else if (command->fd_flag == 2)
 	{
 		double_back_redirect(command, file_name);
-		printf("d-->|%s|\n", *p_command_line);
 	}
 	command->fd_flag = 0;
 	free(file_name);
