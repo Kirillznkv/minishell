81d6c7a9d4cd3c4f9e32a03206d294711f874f4a
kshanti_home <Zenich99@gmail.com>
2021-06-25 14:10:28 +0300

    starting history

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 19:21:27 by kshanti           #+#    #+#             */
-/*   Updated: 2021/06/25 12:43:06 by kshanti          ###   ########.fr       */
+/*   Updated: 2021/06/25 13:48:29 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ int		main(int argc, char **argv, char **env)
 	env_main = ft_create_env(env, new_elem_env());
 	new_env = rewrite_env_parse(env_main);
 	ptr = env_main;
-	write(1, ANSI_COLOR_RED "Minishell-2.🔞$" ANSI_COLOR_RESET, 28);
-	while ((line = readline(" ")))
+	while ((line = readline(ANSI_COLOR_RED "Minishell-2.🔞$ " ANSI_COLOR_RESET)))
 	{
+		if (line && line[0])
+			add_history(line);
 		parser(line, &new_env, env_main);
-		write(1, ANSI_COLOR_RED "Minishell-2.🔞$" ANSI_COLOR_RESET, 28);
 	}
 	while (ptr)
 		ptr = free_t_env(ptr);
