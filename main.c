6e4e4a0fa50388e7ea6e353e831a2b6ce6b19ce9
kshanti_home <Zenich99@gmail.com>
2021-06-25 12:44:10 +0300

    fix clearing start string

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 19:21:27 by kshanti           #+#    #+#             */
-/*   Updated: 2021/06/24 15:49:03 by kshanti          ###   ########.fr       */
+/*   Updated: 2021/06/25 12:43:06 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ int		main(int argc, char **argv, char **env)
 	env_main = ft_create_env(env, new_elem_env());
 	new_env = rewrite_env_parse(env_main);
 	ptr = env_main;
-	write(1, ANSI_COLOR_RED "Minishell-2.🔞$ " ANSI_COLOR_RESET, 28);
-	while ((line = readline(NULL)))
+	write(1, ANSI_COLOR_RED "Minishell-2.🔞$" ANSI_COLOR_RESET, 28);
+	while ((line = readline(" ")))
 	{
 		parser(line, &new_env, env_main);
-		write(1, ANSI_COLOR_RED "Minishell-2.🔞$ " ANSI_COLOR_RESET, 28);
+		write(1, ANSI_COLOR_RED "Minishell-2.🔞$" ANSI_COLOR_RESET, 28);
 	}
 	while (ptr)
 		ptr = free_t_env(ptr);
