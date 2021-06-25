55aaa890d13515ed82464b07cda55a3bc381f73c
kshanti_home <Zenich99@gmail.com>
2021-06-25 15:32:30 +0300

    lalala samolet

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 19:21:27 by kshanti           #+#    #+#             */
-/*   Updated: 2021/06/25 15:26:25 by kshanti          ###   ########.fr       */
+/*   Updated: 2021/06/25 15:31:27 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int		main(int argc, char **argv, char **env)
 	env_main = ft_create_env(env, new_elem_env());
 	new_env = rewrite_env_parse(env_main);
 	ptr = env_main;
-	while ((line = readline("Minishell-2.🔞$ ")))
+	while ((line = readline("Minishell-2.🔞 ⌲ ")))
 	{
 		if (line && line[0])
 			add_history(line);
