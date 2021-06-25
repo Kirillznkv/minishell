d0721cb906f6dc97072f088b954891df0e87b63e
kshanti_home <Zenich99@gmail.com>
2021-06-25 17:13:56 +0300

    add colors and style

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 19:21:27 by kshanti           #+#    #+#             */
-/*   Updated: 2021/06/25 15:31:27 by kshanti          ###   ########.fr       */
+/*   Updated: 2021/06/25 17:12:42 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,14 @@ int		main(int argc, char **argv, char **env)
 	env_main = ft_create_env(env, new_elem_env());
 	new_env = rewrite_env_parse(env_main);
 	ptr = env_main;
+	write(1, "\033[1m" ANSI_COLOR_CYAN "", 10);
 	while ((line = readline("Minishell-2.🔞 ⌲ ")))
 	{
 		if (line && line[0])
 			add_history(line);
+		write(1, "\033[0m" ANSI_COLOR_GREEN "", 10);
 		parser(line, &new_env, env_main);
+		write(1, "\033[1m" ANSI_COLOR_CYAN "", 10);
 	}
 	while (ptr)
 		ptr = free_t_env(ptr);
